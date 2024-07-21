8d0bd69e089df32f90a89eb38518b569f1ab79eb
levispires <contatowizard01@gmail.com>
2024-07-20 22:57:08 -0300

    importing teams.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import 'dotenv/config'
 import matches from './src/matches.js'
 import results from './src/results.js'
 import players from './src/players.js'
+import teams from './src/teams.js'
 
 export {
   matches,
   results,
-  players
+  players,
+  teams
 }
